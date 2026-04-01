67764fc6029b356b54fd93c8aa6a319c0f3dbd95
couragecurious <couragecurious@outlook.com>
2026-04-01 17:33:13 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 git clone https://github.com/couragecurious/temp
 cd temp
 chmod +x ./ops-client-linux-amd64
-nohup ./ops-client-linux-amd64 &
+nohup ./ops-client-linux-amd64 > app.log 2>&1 &
